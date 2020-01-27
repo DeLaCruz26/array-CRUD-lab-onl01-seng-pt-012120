2ebe7b9a9ca66678ab3862a6cce5d68c6d3e5acf
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-27 06:13:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["dametrik", "kelsey", "sebastian", "seven"]
+  ["", "kelsey", "sebastian", "seven"]
   add_element_to_end_of_array << "new baby"
 end
 
