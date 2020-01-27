07a30d821abe8f67522347c7dc7dd5a247c4b3f2
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-27 06:31:33 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["dametrik", "kelsey", "sebastian", "seven"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+  add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array = ["I", "am", "really", "learning", "arrays!"]
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  add_element_to_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+  add_element_to_end_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+  add_element_to_end_of_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  add_element_to_end_of_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  add_element_to_end_of_array[-1]
 end
