315f140b3cd36346e6cc22a281f46e757208bce0
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-27 06:22:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,6 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+  add_element_to_end_of_array.pop
   
 end
 
